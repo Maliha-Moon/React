fe32111202c92249592dfbf9a4b92d70b21623ab
Maliha Rahman Moon <malihamoon05@gmail.com>
2025-03-06 21:08:59 +0600

    add useState concept

--- src/Components/props/State.jsx
+++ src/Components/props/State.jsx
@@ -1,1 +1,20 @@
+import React, { useState } from "react";
+
+const State = () => {
+  //   let array = useState();
+  //   console.log(array);
+
+  const [count, setCount] = useState(5);
+  const handleButtonClick = () => {
+    setCount(() => count + 1);
+  };
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={handleButtonClick}>Increment</button>
+    </>
+  );
+};
+
+export default State;
 
