3da59f6ddb060ee74dfa49d0bfe7c53bc7b36e3b
Maliha Rahman Moon <malihamoon05@gmail.com>
2025-03-08 20:26:54 +0600

    Add common component

--- src/mortageCalculator/components/common/sliderComponent.jsx
+++ src/mortageCalculator/components/common/sliderComponent.jsx
@@ -1,1 +1,51 @@
+import React from "react";
+import Slider from "@mui/material/Slider";
+import { Stack, Typography } from "@mui/material";
+const SliderComponent = ({
+  defaultValue,
+  min,
+  max,
+  amount,
+  label,
+  unit,
+  step,
+  value,
+  onChange,
+}) => {
+  return (
+    <Stack my={1}>
+      <Stack gap={1}>
+        <Typography variant="subtitle2">{label}</Typography>
+        <Typography variant="h5">
+          {unit}
+          {amount}
+        </Typography>
+      </Stack>
+      <Slider
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        value={value}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks
+        step={step}
+        onChange={onChange}
+      />
+
+      <Stack direction="row" justifyContent={"space-between"}>
+        <Typography variant="caption" color="text.secondary">
+          {unit}
+          {min}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          {unit}
+          {max}
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default SliderComponent;
 
